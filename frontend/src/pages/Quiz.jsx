11f0290652639a38d3cbae6da5721b4e2b1d1c26
OcAdriann <ocadriannnc20@gmail.com>
2024-10-22 14:45:17 -0400

    formatted quiz code

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,6 +1,5 @@
 import "../styles/Quiz.css"
 import { useState } from "react";
-import { Link } from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
 
 export default function Quiz() {
