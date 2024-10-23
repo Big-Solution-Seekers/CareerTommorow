de52070d53c6c75f64446c96b03fa1434a7b66e6
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-10-23 15:50:59 -0400

    not done yet

--- frontend/src/components/CarouselComponent.jsx
+++ frontend/src/components/CarouselComponent.jsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 export const useCarousel = (carouselImages) => {
 
   const [pic, setNextPic] = useState(0)
-
+  ç
   const handleClickUp = () => setNextPic(prevIdx => (prevIdx + 1) % carouselImages.length)
 
   const handleClickDown = () => setNextPic(prevIdx => (prevIdx - 1 + carouselImages.length) % carouselImages.length)
