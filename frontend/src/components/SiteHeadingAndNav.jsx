f1d8c95561d2e6c60f2e833500cc1318760d0f7f
Abdullah Khan <164076746+Abdullahk1105@users.noreply.github.com>
2024-10-31 23:13:41 -0400

    added quireis

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -1,32 +1,120 @@
+// import { NavLink } from "react-router-dom";
+// import { useContext } from "react";
+// import CurrentUserContext from "../contexts/current-user-context";
+// import logo from '../images/3.png'
+
+// export default function SiteHeadingAndNav() {
+//   const { currentUser } = useContext(CurrentUserContext);
+
+//   return (
+//     <header>
+//       <a id='logo' href='/'>
+//         <img className="logo-image" src={logo} alt="Logo" />
+//       </a>
+//       <nav>
+//         <ul>
+//           <li><NavLink to='/'>Home</NavLink></li>
+//           <li><NavLink to='/about-us'>About Us</NavLink></li>
+//           {currentUser ? (
+//             <>
+//               <li><NavLink to="/community-forum">Community Forum</NavLink></li>
+//               <li>
+//                 <NavLink to={`/users/${currentUser.id}`} style={{ display: 'flex', alignItems: 'center' }}>
+//                   {currentUser.username}
+//                   {/* Display the profile image after the username */}
+//                   {currentUser.profileImage && (
+//                     <img
+//                       src={currentUser.profileImage} // Assuming this is the URL of the profile image
+//                       alt={`${currentUser.username}'s profile`}
+//                       className="profile-image" // Use the defined class for styling
+//                     />
+//                   )}
+//                 </NavLink>
+//               </li>
+//             </>
+//           ) : (
+//             <>
+//               <li><NavLink to='/login'>Login</NavLink></li>
+//               <li><NavLink to='/sign-up'>Sign Up</NavLink></li>
+//             </>
+//           )}
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
-import logo from '../images/3.png'
+import logo from '../images/3.png';
 
 export default function SiteHeadingAndNav() {
   const { currentUser } = useContext(CurrentUserContext);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(prevState => !prevState);
+  };
 
   return (
-    <header>
-      <a id='logo' href='/'>
-        <img className="logo-image" src={logo} alt="Logo" />
-      </a>
-      <nav>
+    <>
+      <header>
+        <a id='logo' href='/'>
+          <img className="logo-image" src={logo} alt="Logo" />
+        </a>
+        <div className="hamburger" onClick={toggleSidebar}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        {/* Nav is initially hidden on mobile and will be shown on desktop */}
+        <nav className={`navbar ${isSidebarOpen ? 'open' : ''}`}>
+          <ul>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/about-us'>About Us</NavLink></li>
+            {currentUser ? (
+              <>
+                <li><NavLink to="/community-forum">Community Forum</NavLink></li>
+                <li>
+                  <NavLink to={`/users/${currentUser.id}`} style={{ display: 'flex', alignItems: 'center' }}>
+                    {currentUser.username}
+                    {currentUser.profileImage && (
+                      <img
+                        src={currentUser.profileImage}
+                        alt={`${currentUser.username}'s profile`}
+                        className="profile-image"
+                      />
+                    )}
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li><NavLink to='/login'>Login</NavLink></li>
+                <li><NavLink to='/sign-up'>Sign Up</NavLink></li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </header>
+
+      {/* Sidebar */}
+      <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
         <ul>
-          <li><NavLink to='/'>Home</NavLink></li>
-          <li><NavLink to='/about-us'>About Us</NavLink></li>
+          <li><NavLink to='/' onClick={toggleSidebar}>Home</NavLink></li>
+          <li><NavLink to='/about-us' onClick={toggleSidebar}>About Us</NavLink></li>
           {currentUser ? (
             <>
-              <li><NavLink to="/community-forum">Community Forum</NavLink></li>
+              <li><NavLink to="/community-forum" onClick={toggleSidebar}>Community Forum</NavLink></li>
               <li>
-                <NavLink to={`/users/${currentUser.id}`} style={{ display: 'flex', alignItems: 'center' }}>
+                <NavLink to={`/users/${currentUser.id}`} onClick={toggleSidebar} style={{ display: 'flex', alignItems: 'center' }}>
                   {currentUser.username}
-                  {/* Display the profile image after the username */}
                   {currentUser.profileImage && (
                     <img
-                      src={currentUser.profileImage} // Assuming this is the URL of the profile image
+                      src={currentUser.profileImage}
                       alt={`${currentUser.username}'s profile`}
-                      className="profile-image" // Use the defined class for styling
+                      className="profile-image"
                     />
                   )}
                 </NavLink>
@@ -34,12 +122,12 @@ export default function SiteHeadingAndNav() {
             </>
           ) : (
             <>
-              <li><NavLink to='/login'>Login</NavLink></li>
-              <li><NavLink to='/sign-up'>Sign Up</NavLink></li>
+              <li><NavLink to='/login' onClick={toggleSidebar}>Login</NavLink></li>
+              <li><NavLink to='/sign-up' onClick={toggleSidebar}>Sign Up</NavLink></li>
             </>
           )}
         </ul>
-      </nav>
-    </header>
+      </div>
+    </>
   );
 }
