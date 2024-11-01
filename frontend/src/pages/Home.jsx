4fc4112053b7a3c31ae4d41c0d9c583469d7544b
Rashell <rashellaldas@rashells-mbp.mynetworksettings.com>
2024-10-31 20:20:18 -0400

    nav bar css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 
 export default function HomePage() {
   useEffect(() => {
+    document.body.style.backgroundColor = 'rgb(172, 208, 230)'; // Your starting color
     // ScrollReveal animations
     ScrollReveal().reveal('.carousel', {
       origin: 'bottom',
